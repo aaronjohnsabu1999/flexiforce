d75cf5ecbead0981968402d2feeebacd46a37c6f
Ben Forbes <benjaminforbes@me.com>
2025-06-08 12:59:46 -0700

    added input and outputs to interface with opensim

--- controller.py
+++ controller.py
@@ -3,7 +3,7 @@ import mujoco
 from scipy.spatial.transform import Rotation as R
 
 class AdmittanceController:
-    def __init__(self, model, data, site_name, M, B, K, Kp, Kd):
+    def __init__(self, model, data, site_name, M, B, K, Kp, Kd, desired_activation):
         self.model = model
         self.data = data
         self.M = M
@@ -12,25 +12,39 @@ class AdmittanceController:
         self.Kp = Kp
         self.Kd = Kd
         self.site_name = site_name
+        self.desired_activation = desired_activation
 
         self.site_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, site_name)
 
         self.x = np.zeros(6)
         self.xd = np.zeros(6)
         self.xdd = np.zeros(6)
-        # self.force_measured = np.zeros(6)
         self.external_force = np.zeros(6)
 
         self.traj_func = None
         self.vel_func = None
 
     def set_external_force(self, force,t):
-        self.external_force = np.sin(t)*np.array(force) # TODO: ADD THE FORCE APPLIED by the EMG HERE
+        self.external_force = np.array(force) 
 
     def set_trajectory(self, traj_func, vel_func):
         self.traj_func = traj_func
         self.vel_func = vel_func
 
+    def force_on_patient(self, pos_err, K): # TODO: connect this to the input of the OpenSim model
+        F = pos_err @ K
+        return F
+    
+    def set_K(self, simulated_activation, desired_activation):
+        activation_err = desired_activation - simulated_activation # this is an arbitrary control law for changing K
+        scale = (1 + 0.001* activation_err)
+        K_new = self.K * scale
+        return K_new
+    
+    def opensim_simulated_activation(self,t): #TODO: connect this to the output of the OpenSim model
+        fake = 1.0 + 0.1*np.sin(t) # For Ben's debugging
+        return fake
+    
     def get_current_pose(self):
         pos = self.data.site_xpos[self.site_id].copy()
         mat = self.data.site_xmat[self.site_id].reshape((3, 3))
@@ -52,8 +66,16 @@ class AdmittanceController:
         pos_err = x_meas - x_ref
         vel_err = self.xd - xd_ref
 
-        rhs = self.external_force - self.B @ vel_err - self.K @ pos_err
-        self.xdd = np.linalg.solve(self.M, rhs.reshape(-1, 1)).flatten()
+        self.simulated_activation = self.opensim_simulated_activation(t)
+
+        self.K = self.set_K(self.simulated_activation, self.desired_activation)
+        print('K is:')
+        print(self.K)
+
+        external_force = -self.force_on_patient(pos_err, self.K) # we are updating the external force applied to the simulation as well
+
+        rhs = external_force - self.B @ vel_err - self.K @ pos_err
+        self.xdd = np.linalg.solve(self.M, rhs)
 
         self.xd += self.xdd * dt
         self.x += self.xd * dt
--- main.py
+++ main.py
@@ -15,17 +15,31 @@ def load_config():
 def plot_results(log):
     plt.figure(figsize=(15, 8))
 
-    plt.subplot(2, 1, 1)
-    for i, label in enumerate(['X', 'Y', 'Z']):
-        plt.plot(log["time"], log["x_ref"][:, i], linestyle='--', label=f"{label} Reference")
-        plt.plot(log["time"], log["measured_position"][:, i], label=f"{label} Measured")
+    plt.subplot(3, 1, 1)
+    plt.title('End Effector Trajectory')
+    colors = ['r', 'g', 'b']
+    for i, (label, color) in enumerate(zip(['X', 'Y', 'Z'], colors)):
+        plt.plot(log["time"], log["x_ref"][:, i], linestyle='--', color=color, label=f"{label} Reference")
+        plt.plot(log["time"], log["measured_position"][:, i], color=color, label=f"{label} Measured")
     plt.ylabel("Position (m)")
     plt.legend()
     plt.grid(True)
 
-    plt.subplot(2, 1, 2)
-    plt.plot(log["time"], log["applied_force"][:, 2], label="Applied External Z Force")
+    plt.subplot(3, 1, 2)
+    plt.title('Force Applied on Patient')
+    plt.plot(log["time"], log["applied_force"][:, 0], label="Applied X Force")
+    plt.plot(log["time"], log["applied_force"][:, 1], label="Applied Y Force")
+    plt.plot(log["time"], log["applied_force"][:, 2], label="Applied Z Force")
     plt.ylabel("Force (N)")
+    plt.xlabel("Time (s)")
+    plt.grid(True)
+    plt.legend()
+
+    plt.subplot(3, 1, 3)
+    plt.title('Activation')
+    plt.plot(log["time"], log["desired_activation"], label="Desired Activation")
+    plt.plot(log["time"], log["simulated_activation"], label="Simulated Activation")
+    plt.ylabel("Activation (A.U.)")
     plt.xlabel("Time (s)")
     plt.grid(True)
     plt.legend()
@@ -51,20 +65,20 @@ def run():
         B=np.diag(config["admittance_controller"]["B"]),
         K=np.diag(config["admittance_controller"]["K"]),
         Kp=config["admittance_controller"]["Kp"],
-        Kd=config["admittance_controller"]["Kd"]
+        Kd=config["admittance_controller"]["Kd"],
+        desired_activation=config["forces"]["desired_activation"]
     )
 
     data.qpos[:] = np.array(config["simulation"]["initial_qpos"])
     data.qvel[:] = 0.0
     mujoco.mj_forward(model, data)
 
-    controller.set_external_force(config["forces"]["external_force"],0)
     traj_func, vel_func = get_trajectory_functions(config)
     controller.set_trajectory(traj_func, vel_func)
 
     dt = config["simulation"]["dt"]
     duration = config["simulation"]["duration"]
-    log = {"time": [], "force": [], "vel": [], "position":[], "applied_force": [], "measured_position": [], "x_ref": []}
+    log = {"time": [], "vel": [], "position":[], "applied_force": [], "measured_position": [], "x_ref": [], "desired_activation":[], "simulated_activation":[]}
 
     site_id = mujoco.mj_name2id(model, mujoco.mjtObj.mjOBJ_SITE, config["simulation"]["site_name"])
     body_id = model.site_bodyid[site_id]
@@ -73,31 +87,31 @@ def run():
         t0 = time.time()
         while viewer.is_running() and time.time() - t0 < duration:
             t = time.time() - t0
-            controller.set_external_force(config["forces"]["external_force"],t)
             tau = controller.compute_torques(t, dt)
             data.ctrl[:] = tau
             mujoco.mj_step(model, data)
             viewer.sync()
 
             log["time"].append(t)
-            log["force"].append(controller.external_force.copy())
             log["vel"].append(data.qvel.copy())
             log["position"].append(controller.x[:3].copy())
-            log["applied_force"].append(controller.external_force.copy())
 
             _, _, pose_meas = controller.get_current_pose()
             log["measured_position"].append(pose_meas[:3].copy())
             x_ref, _ = controller.get_reference_at_time(t)
             log["x_ref"].append(x_ref[:3].copy())
-            time.sleep(0.001)
+            log["applied_force"].append(controller.force_on_patient(controller.x - x_ref, controller.K).copy())
+            log["desired_activation"].append(controller.desired_activation)
+            log["simulated_activation"].append(controller.simulated_activation)
 
     log["time"] = np.array(log["time"])
-    log["force"] = np.vstack(log["force"])
     log["vel"] = np.vstack(log["vel"]) if log["vel"] else np.array([])
     log["position"] = np.array(log["position"])
     log["applied_force"] = np.vstack(log["applied_force"])
     log["measured_position"] = np.array(log["measured_position"])
     log["x_ref"] = np.array(log["x_ref"])
+    log["desired_activation"] = np.array(log["desired_activation"])
+    log["simulated_activation"] = np.array(log["simulated_activation"])
 
     plot_results(log)
 
